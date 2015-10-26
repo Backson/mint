9e807921a521c32018fbd68f222233ecc989873e
Lars Drögemüller <larstd90@gmail.com>
2015-10-26 17:39:08 +0100

    Commented the optimization passes

--- src/optimizations.hpp
+++ src/optimizations.hpp
@@ -12,9 +12,14 @@ public:
 	Optimizer() = default;
 	~Optimizer() = default;
 	
+	/// Substitute calls to pow(double,double) with cheaper pow(double,int)
 	void optimizePowersToIntegerExponents(Ast *);
+	/// collapses sub-expressions that do not depend on any arguments into a single constant
 	void collapseConstants(Ast *);
+	/// -(-(x)) -> x
 	void collapseSigns(Ast *);
+	/// rebalances the AST, so calculations which require lots of space are done first.  This
+	/// can sometimes reduce the total necessary amount of space on the stack.
 	void compressStack(Ast *);
 
 	void optimizeDefaults(Ast *);
